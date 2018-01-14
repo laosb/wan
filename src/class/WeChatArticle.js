4834750060857bbcd93093ed497a3f448580d944
laosb <laosb@lasteam.net>
2018-01-14 15:55:57 +0800

    [doc] Fixed some doc problems.

--- src/class/WeChatArticle.js
+++ src/class/WeChatArticle.js
@@ -10,9 +10,9 @@ import { VM } from 'vm2'
 export default class WeChatArticle {
   /**
    * Take the url. Just note the article won't be actually loaded and parsed
-   * before you call `WeChatArticle#fetchAndParse` manually.
+   * before you call {@link WeChatArticle#fetchAndParse} manually.
    *
-   * You could see if this article is parsed or not, by using `WeChatArticle#isParsed()`.
+   * You could see if this article is parsed or not, by using {@link WeChatArticle#isParsed}.
    *
    * @param {String} url - A valid WeChat article URL, begins with
    * http(s)://mp.weixin.qq.com/s
@@ -22,13 +22,18 @@ export default class WeChatArticle {
       !url.startsWith('http://mp.weixin.qq.com/s')) {
       throw new Error('url is not a WeChat article url')
     }
+
+    /** @type {String} */
     this.url = url
+
+    /** @type {Boolean} */
     this._parsed = false
   }
+
   /**
    * See if this article has been loaded & parsed.
    *
-   * Load & parse with `WeChatArticle#fetchAndParse()`.
+   * Load & parse with {@link WeChatArticle#fetchAndParse}.
    *
    * @return {Boolean} Parsed or not.
    */
@@ -60,15 +65,33 @@ export default class WeChatArticle {
       __getInfoFunc();
     `
     const infoObj = (new VM()).run(usefulCode)
+
+    /** @type {String} */
     this.title = infoObj.title
+
+    /** @type {Date} */
     this.publishedAt = moment($('#post-date').text().trim()).toDate()
     const authorEl = $('#meta_content > em:nth-child(2)')
-    this.author = authorEl ? authorEl.text().trim() : ''
+
+    /**
+     * The author. Only exists when it does have an author field.
+     *
+     * To get a must-have author name, use {@link WeChatArticle.getAuthorName}.
+     * @type {String}
+     */
+    this.author = authorEl ? authorEl.text().trim() : undefined
     this.account = infoObj.account
+
+    /**
+     * The original content fetched from WeChat public platform. Actually from
+     * `#js-content` part of the whole raw HTML.
+     * @type {String}
+     */
     this.content = $('#js_content').html()
     this._parsed = true
     return true
   }
+
   /**
    * Returns author's name if the author does specify an author name on the
    * article, or the name of the Official Account if not.
