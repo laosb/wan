8ff3312701fb3bbee5cac6e47cbc619b5a91974e
laosb <laosb@lasteam.net>
2018-01-16 09:57:40 +0800

    Improved normalization.

--- src/normalize.js
+++ src/normalize.js
@@ -14,12 +14,23 @@ import { cleanStyle } from './lib/style'
  * @since 0.3.0
  */
 export function normalize ($) {
+  $('br').each((i, el) => {
+    while ($(el).parent().children().length === 1) {
+      const $p = $(el).parent()
+      $(el).insertAfter($(el).parent())
+      $p.remove()
+    }
+  }) // Unwrap <br>s from any kind of element that only contains a <br>.
   $('[style]').each((i, el) => $(el).attr('style', cleanStyle($(el).attr('style'))))
   $('[powered-by]').attr('powered-by', null)
   $('[class="Powered-by-XIUMI V5"]').attr('class', null)
+  $('[data-label="powered by xmt.cn"]').remove()
   $('[class=""]').attr('class', null)
   $('section:not([class],[style])').each((e, el) => {
-    if ($(el).children().length <= 1) $(el).remove()
-  })
+    if ($(el).children().length === 1) {
+      $(el).children()[0].insertAfter($(el))
+      $(el).remove()
+    }
+  }) // Unwrap elements in nonsense sections.
   return $
 }
